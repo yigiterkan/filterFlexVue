1b90fc7f05881c93458c5677dea9d8f3739ec2ab
Yiğit Cumhur Erkan <79741387+yigiterkan@users.noreply.github.com>
2023-10-18 22:04:51 +0300

    Second commit

--- filterFlexVue/src/views/DataTable.vue
+++ filterFlexVue/src/views/DataTable.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="flex">
-    <!-- Filters -->
     <div class="filter-section">
       <el-input v-model="searchDate" placeholder="Search by Date Range" class="filter-input"></el-input>
       <el-date-picker v-model="startDate" type="date" placeholder="Start Date"></el-date-picker>
@@ -13,15 +12,27 @@
       <el-button type="success">Download</el-button>
     </div>
 
-    <!-- Table -->
     <el-table :data="filteredTableData"  style="width: 100%">
       <el-table-column prop="CustNr" label="Kunde-Nr" width="180"></el-table-column>
       <el-table-column prop="FULLNAME" label="Name" width="180"></el-table-column>
       <el-table-column prop="Fachrichtung" label="Fachrichtung" width="180"></el-table-column>
       <el-table-column prop="City" label="City" width="180"></el-table-column>
       <el-table-column prop="eMail" label="Email" width="180"></el-table-column>
+      <el-table-column label="Auftrag" width="180">
+        <template slot-scope="scope">
+          <el-button @click="showOrderDetails(scope.row)" type="text">{{ scope.row.OrderNr }}</el-button>
+        </template>
+      </el-table-column>
       <el-table-column prop="ORDERDATE" label="Datum" width="180"></el-table-column>
+      <el-table-column prop="status" label="Status" width="180"></el-table-column>
     </el-table>
+    <el-dialog :visible.sync="orderDialogVisible" width="50%">
+      <div v-if="selectedOrder">
+        <h3>Order Details</h3>
+        <p><strong>Order Number:</strong> {{ selectedOrder.OrderNr }}</p>
+        <!-- Add other order details as required -->
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -35,7 +46,9 @@ export default {
       endDate: '',
       customerNumber: '',
       tableData: oxidTestData,
-      filteredTableData: oxidTestData
+      filteredTableData: oxidTestData,
+      orderDialogVisible: false,
+      selectedOrder: null
     };
   },
   methods: {
@@ -50,7 +63,19 @@ export default {
     },
     filterByCustomerNumber() {
       this.filteredTableData = this.tableData.filter(row => row.CustNr.includes(this.customerNumber));
+    },
+    showOrderDetails(order) {
+      this.selectedOrder = order;
+      this.orderDialogVisible = true;
+    },
+    rowClassName({ row }) {
+    if (row.status === 1) {
+      return 'status-warning';
+    } else if (row.status === 3) {
+      return 'status-success';
     }
+    return '';
+  }
   }
 };
 </script>
@@ -72,5 +97,12 @@ export default {
   align-items: center;
   height: 100vh;
   flex-direction: column;  
+}
+.status-warning {
+  background-color: var(--el-color-warning-light-9);
+}
+
+.status-success {
+  background-color: var(--el-color-success-light-9);
 }
 </style>
